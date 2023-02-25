d5505fad31f59076403511dc833a59373863c661
Paolo Debuque <pdebuque@gmail.com>
2023-02-25 11:41:22 -1000

    create and format square inputs

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import {SquareInt} from '../model'
+import { SquareInt } from '../model'
 
 interface Props {
   square: SquareInt;
@@ -8,12 +8,31 @@ interface Props {
 
 const Square: React.FC<Props> = (props) => {
 
-const {
-  square
-} = props
+  const {
+    square
+  } = props
+
+  const [focus, setFocus] = useState<boolean>(false)
+
+  const handleClick: (e: any) => void = (e) => {
+    console.log('clicked square: ', square)
+    setFocus(true)
+  }
 
   return (
-    <div className = 'square'>{square.value}</div>
+    <div
+      className='square'
+      onClick={handleClick}
+    >{
+        focus ?
+          <input
+            className='square-input'
+            type='number'
+            />
+          :
+          square.value
+      }
+    </div>
   )
 }
 
