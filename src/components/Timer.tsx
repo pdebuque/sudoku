897f8f3d90aac53d66290d193a2251babf30b477
Paolo Debuque <pdebuque@gmail.com>
2023-03-04 15:06:28 -0600

    minor styling

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import { DateTime, Duration } from 'luxon'
+
+interface Props {
+  start: DateTime;
+  time: DateTime;
+
+}
+
 const Timer = () => {
   return (
     <div>Timer</div>
